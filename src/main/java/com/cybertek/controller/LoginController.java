5d60b1c0eca81687cf31e9884bf0aa925853be77
kamardinof <kamardinof@gmail.com>
2021-05-09 00:21:10 -0400

    added more config

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	
-	@RequestMapping
+	@RequestMapping(value = "/login")
 	public String login(){
-		return "login";
+		return "/login";
 	}
 	
 	@RequestMapping("/welcome")
